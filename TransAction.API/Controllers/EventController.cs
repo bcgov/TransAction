35dd4aa5bbc796e404c95077a62b2fda387a8082
AbhinavTheOG <48139542+AbhinavTheOG@users.noreply.github.com>
2019-03-13 10:29:41 -0700

    Initial Commit

---
+++ TransAction.API/Controllers/EventController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TransActionPractice.Models;
+
+namespace TransActionPractice.Controllers
+{
+    [Route("api/events")]
+    public class EventController : Controller
+    {
+        private TransActionPracticeContext _context;
+
+        public EventController(TransActionPracticeContext context)
+        {
+            _context = context;
+        }
+                       
+        [HttpPost()]
+        public async Task<ActionResult<TraEvent>> PostEvent(TraEvent item)
+        {
+            _context.TraEvent.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetEvents), new { id = item.EventId }, item);
+        }
+
+        [HttpGet()]
+        public IActionResult GetEvents()
+        {
+
+            if (_context.TraEvent == null)
+            {
+                return NotFound();
+            }
+            return Ok(_context.TraEvent);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEvent(int id)
+        {
+            var EventD = _context.TraEvent.FirstOrDefault(c => c.EventId == id);
+            if (EventD == null)
+            {
+                return NotFound();
+            }
+            return Ok(EventD);
+        }
+    }
+}
