9e57a9d4f155ad6b21679433b6514b129dfcd1a7
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2019-03-14 11:45:47 -0700

    Added client build and deploy scripts

--- .pipeline/lib/deploy.js
+++ .pipeline/lib/deploy.js
@@ -1,38 +1,60 @@
-'use strict';
+"use strict";
 
-const { OpenShiftClientX } = require('pipeline-cli');
-const path = require('path');
+const { OpenShiftClientX } = require("pipeline-cli");
+const path = require("path");
 
-module.exports = (settings) => {
+module.exports = settings => {
   const phase = settings.phase;
   const phases = settings.phases;
 
   const oc = new OpenShiftClientX({ namespace: phases[phase].namespace });
-  const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
+  const templatesLocalBaseUrl = oc.toFileUrl(
+    path.resolve(__dirname, "../../openshift")
+  );
   //const templateFile = path.resolve(__dirname, '../../openshift/client-deploy-config.yaml');
 
   let objects = [];
 
-  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/client-deploy-config.yaml`, {
-    param: {
-      NAME: `${phases[phase].name}-client`,
-      SUFFIX: phases[phase].suffix,
-      VERSION: phases[phase].tag,
-      ASPNETCORE_ENVIRONMENT: phases[phase].dotnet_env
-    },
-  }))
+  objects = objects.concat(
+    oc.processDeploymentTemplate(
+      `${templatesLocalBaseUrl}/client-deploy-config.yaml`,
+      {
+        param: {
+          NAME: `${phases[phase].name}-client`,
+          SUFFIX: phases[phase].suffix,
+          VERSION: phases[phase].tag,
+          ASPNETCORE_ENVIRONMENT: phases[phase].dotnet_env
+        }
+      }
+    )
+  );
 
-  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/api-deploy-config.yaml`, {
-    param: {
-      NAME: `${phases[phase].name}-api`,
-      SUFFIX: phases[phase].suffix,
-      VERSION: phases[phase].tag,
-      ASPNETCORE_ENVIRONMENT: phases[phase].dotnet_env
-    },
-  }))
+  objects = objects.concat(
+    oc.processDeploymentTemplate(
+      `${templatesLocalBaseUrl}/api-deploy-config.yaml`,
+      {
+        param: {
+          NAME: `${phases[phase].name}-api`,
+          SUFFIX: phases[phase].suffix,
+          VERSION: phases[phase].tag,
+          ASPNETCORE_ENVIRONMENT: phases[phase].dotnet_env
+        }
+      }
+    )
+  );
 
-  oc.applyRecommendedLabels(objects, phases[phase].name, phase, phases[phase].changeId);
+  oc.applyRecommendedLabels(
+    objects,
+    phases[phase].name,
+    phase,
+    phases[phase].changeId
+  );
   oc.fetchSecretsAndConfigMaps(objects);
-  oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag);
+  oc.importImageStreams(
+    objects,
+    phases[phase].tag,
+    phases.build.namespace,
+    phases.build.tag
+  );
   oc.applyAndDeploy(objects, phases[phase].instance);
 };
