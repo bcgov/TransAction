b993dddfe51340c7d0af1981d7a8a2c964cd2a96
AbhinavTheOG <48139542+AbhinavTheOG@users.noreply.github.com>
2019-03-22 15:41:40 -0700

    Updated the tables in the sql project

--- TransAction.SQL/dbo/Tables/TRA_TOPIC.sql
+++ TransAction.SQL/dbo/Tables/TRA_TOPIC.sql
@@ -2,12 +2,11 @@
     [TOPIC_ID]      INT            IDENTITY (1, 1) NOT NULL,
     [TITLE]   VARCHAR(1024) NOT NULL,
     [USER_ID] INT            NOT NULL,
-	[EFFECTIVE_START_DATE] DATETIME NULL, 
-	[EFFECTIVE_END_DATE] DATETIME NULL, 
-	[CREATED_BY_USER] VARCHAR(255) NULL, 
-	[CREATED_BY_DATE] DATETIME NULL, 
-	[LAST_UPDATED_BY_USER] VARCHAR(255) NULL, 
-	[LAST_UPDATED_BY_DATE] DATETIME NULL,
+	[DB_CREATE_TIMESTAMP] DATETIME NOT NULL, 
+	[DB_CREATE_USERID] VARCHAR(30) NOT NULL, 
+	[DB_LAST_UPDATE_TIMESTAMP] DATETIME NOT NULL, 
+	[DB_LAST_UPDATE_USERID] VARCHAR(30) NOT NULL,
+    [CONCURRENCY_CONTROL_NUMBER] BIGINT NOT NULL DEFAULT 1, 
     CONSTRAINT [PK_TOPIC] PRIMARY KEY CLUSTERED ([TOPIC_ID] ASC),
     CONSTRAINT [FK_TOPIC_USER] FOREIGN KEY ([USER_ID]) REFERENCES [dbo].[TRA_USER] ([USER_ID])
 );
@@ -17,3 +16,40 @@ GO
 CREATE NONCLUSTERED INDEX [IX_FK_TOPIC_USER]
     ON [dbo].[TRA_TOPIC_MESSAGE]([USER_ID] ASC);
 
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The date and time the record was created.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TRA_TOPIC',
+    @level2type = N'COLUMN',
+    @level2name = N'DB_CREATE_TIMESTAMP'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The user or proxy account that created the record. **',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TRA_TOPIC',
+    @level2type = N'COLUMN',
+    @level2name = N'DB_CREATE_USERID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The date and time the record was created or last updated.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TRA_TOPIC',
+    @level2type = N'COLUMN',
+    @level2name = N'DB_LAST_UPDATE_TIMESTAMP'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The user or proxy account that created or last updated the record. **',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TRA_TOPIC',
+    @level2type = N'COLUMN',
+    @level2name = N'DB_LAST_UPDATE_USERID'
--- TransAction.SQL/dbo/Tables/TRA_USER_ACTIVITY.sql
+++ TransAction.SQL/dbo/Tables/TRA_USER_ACTIVITY.sql
@@ -4,12 +4,10 @@
     [HOURS]       FLOAT NOT NULL,
     [USER_ID]      INT            NOT NULL,
     [ACTIVITY_ID] INT            NOT NULL,
-	[EFFECTIVE_START_DATE] DATETIME NULL, 
-	[EFFECTIVE_END_DATE] DATETIME NULL, 
-	[CREATED_BY_USER] VARCHAR(255) NULL, 
-	[CREATED_BY_DATE] DATETIME NULL, 
-	[LAST_UPDATED_BY_USER] VARCHAR(255) NULL, 
-	[LAST_UPDATED_BY_DATE] DATETIME NULL,
+	[DB_CREATE_TIMESTAMP] DATETIME NOT NULL, 
+	[DB_CREATE_USERID] VARCHAR(30) NOT NULL, 
+	[DB_LAST_UPDATE_TIMESTAMP] DATETIME NOT NULL, 
+	[DB_LAST_UPDATE_USERID] VARCHAR(30) NOT NULL,
     CONSTRAINT [PK_USER_ACTIVITY] PRIMARY KEY CLUSTERED ([USER_ACTIVITY_ID] ASC),
     CONSTRAINT [FK_USER_ACTIVITY_ACTIVITY] FOREIGN KEY ([ACTIVITY_ID]) REFERENCES [dbo].[TRA_ACTIVITY] ([ACTIVITY_ID]),
     CONSTRAINT [FK_USER_ACTIVITY_USER] FOREIGN KEY ([USER_ID]) REFERENCES [dbo].[TRA_USER] ([USER_ID])
@@ -25,3 +23,40 @@ GO
 CREATE NONCLUSTERED INDEX [IX_FK_USER_ACTIVITY_ACTIVITY]
     ON [dbo].[TRA_USER_ACTIVITY]([ACTIVITY_ID] ASC);
 
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The date and time the record was created.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TRA_USER_ACTIVITY',
+    @level2type = N'COLUMN',
+    @level2name = N'DB_CREATE_TIMESTAMP'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The user or proxy account that created the record. **',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TRA_USER_ACTIVITY',
+    @level2type = N'COLUMN',
+    @level2name = N'DB_CREATE_USERID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The date and time the record was created or last updated.',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TRA_USER_ACTIVITY',
+    @level2type = N'COLUMN',
+    @level2name = N'DB_LAST_UPDATE_TIMESTAMP'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'The user or proxy account that created or last updated the record. **',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TRA_USER_ACTIVITY',
+    @level2type = N'COLUMN',
+    @level2name = N'DB_LAST_UPDATE_USERID'
