6289a686597b0f7eb868f812c80971d6c8dc0011
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2019-04-15 14:59:28 -0700

    Update deploy.js

--- .pipeline/deploy.js
+++ .pipeline/deploy.js
@@ -4,4 +4,4 @@ const deploy = require('./lib/deploy.js');
 const phases = require('./lib/config.js')
 const options= require('pipeline-cli').Util.parseArguments()
 
-deploy({phases:phases, phase: options.phase});
+deploy({phases:phases, phase: options.env});
