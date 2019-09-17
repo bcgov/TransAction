de1fef29506eeec3bb1c38c965093790bb204875
Neal Li <neal.li@gov.bc.ca>
2019-09-17 10:09:49 -0700

    Updating static content

--- transaction-client/src/js/components/Home.js
+++ transaction-client/src/js/components/Home.js
@@ -39,20 +39,22 @@ class Home extends React.Component {
             <Col>
               <h4 className="text-center">Welcome to TransAction</h4>
               <p>
-                TransAction (Healthy Workplace Challenge) is a voluntary initiative sponsored by the ministry’s Employee
-                Advisory Forum (EAF) and ministry executive as a fun, dynamic way of getting active during Healthy
-                Workplace Month (October). Ministry employees are encouraged to form teams of five with colleagues and
-                earn “fitness points”. Not only do teams compete against one another, but individual time will also help
-                his/or geographic area compete in the 4-way battle between regions and headquarters.
+                TransAction is a voluntary initiative sponsored by the ministry’s Employee Advisory Forum (EAF) and
+                ministry executive as a fun, dynamic way of getting active during Healthy Workplace Month (October).
+                Ministry employees are encouraged to form teams of five colleagues and earn “fitness points.” Not only
+                do teams compete against one another, but individual time will also help each geographic area compete in
+                the four-way battle between regions and headquarters.
               </p>
 
               <p>
-                A ministry employee participates in TransAction by registering and becoming a member of a team (as a
-                team leader or regular team member). Each individual enters daily physical activity time on the
-                TransAction website based on level of intensity and in 15-minute increments, and points are calculated.
-                Prizes are awarded to the highest ranking team. There are also random prize draws throughout the
-                duration of the program. The next TransAction is scheduled to take place in October 2017, this program
-                has been running since October 2010 and we hope to support this initiative for many years to come.
+                To participate in TransAction, you will need to register into one of two categories. If you are
+                competitive by nature and you strive to burn calories you will enter the Competitive category. If you
+                are hoping to increase your activity but prefer a less competitive challenge, then you will register
+                into the Recreational category. Each day you will enter your physical activity time on the TransAction
+                website based on level of intensity and in 15-minute increments. Your fitness points will be
+                automatically calculated. Prizes will be awarded to the top three highest ranking teams in both the
+                Competitive and Recreational categories. There will also be a participation prize drawn every week so,
+                if you have joined a TransAction team, you will be eligible to win the weekly prizes.
               </p>
               <div className="text-center mt-5">
                 <Link to={Constants.PATHS.START}>
