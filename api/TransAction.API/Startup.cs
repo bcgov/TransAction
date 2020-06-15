fb0146189f9c5382fe8f67c175798b2f06d9a3b9
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2020-06-15 14:38:48 -0700

    Update Startup.cs

--- api/TransAction.API/Startup.cs
+++ api/TransAction.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
