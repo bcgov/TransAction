3f4eaef7ff38711beb890a6c7dd80c68984ebc53
bcgov-brwang <bcgov.brwang@gmail.com>
2022-08-15 10:06:17 -0700

    Remove comments

--- client/src/index.js
+++ client/src/index.js
@@ -34,17 +34,6 @@ import { UPDATE_AUTH_USER } from './js/actions/types';
 
 initFontAwesome();
 
-//bruce test 0815
-// const keycloakConfig = {
-//   url: window.RUNTIME_REACT_APP_SSO_HOST ? window.RUNTIME_REACT_APP_SSO_HOST : process.env.REACT_APP_SSO_HOST,
-//   realm: window.RUNTIME_REACT_APP_SSO_REALM ? window.RUNTIME_REACT_APP_SSO_REALM : process.env.REACT_APP_SSO_REALM,
-//   clientId: window.RUNTIME_REACT_APP_SSO_CLIENT
-//     ? window.RUNTIME_REACT_APP_SSO_CLIENT
-//     : process.env.REACT_APP_SSO_CLIENT,
-// };
-
-// const keycloak = Keycloak(keycloakConfig);
-
 const ssoConfig = {
   url: window.RUNTIME_REACT_APP_SSO_HOST ? window.RUNTIME_REACT_APP_SSO_HOST : process.env.REACT_APP_SSO_HOST,
   realm: window.RUNTIME_REACT_APP_SSO_REALM ? window.RUNTIME_REACT_APP_SSO_REALM : process.env.REACT_APP_SSO_REALM,
@@ -71,7 +60,6 @@ function getKeycloakUserInfo() {
   });
 }
 
-//bruce test 0815
 var initOptions = {
   onLoad: 'login-required',
   checkLoginIframe: false,
@@ -79,8 +67,6 @@ var initOptions = {
 };
 
 keycloak
-  //bruce test 0815
-  //.init({ onLoad: 'login-required', checkLoginIframe: false })
   .init(initOptions)
   .success((authenticated) => {
     if (authenticated) {
